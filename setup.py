f21c962a7e3c3005cf0c4541cc8339da50e85b01
maximebf <maxime.bouroumeau@gmail.com>
2014-10-23 00:36:19 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 from setuptools import setup, find_packages
 
 
-def desc():
-    with open("README.md") as f:
-        return f.read()
-
-def reqs():
-    with open('requirements.txt') as f:
-        return f.read().splitlines()
-
 setup(
     name='frasco-forms',
     version='0.1',
@@ -17,10 +9,15 @@ setup(
     author='Maxime Bouroumeau-Fuseau',
     author_email='maxime.bouroumeau@gmail.com',
     description="WTForms integration for Frasco",
-    long_description=desc(),
     packages=find_packages(),
-    include_package_data=True,
+    package_data={
+        'frasco_forms': ['*.html']
+    },
     zip_safe=False,
     platforms='any',
-    install_requires=reqs() + ['frasco']
+    install_requires=[
+        # 'frasco',
+        'Flask-WTF>=0.9.5',
+        'inflection'
+    ]
 )
